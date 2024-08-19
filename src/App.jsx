1f9c824b5a8540fe707d2636f1fb3fb81b4add73
JD Tadlock <jt7903@gmail.com>
2024-08-19 14:36:46 -0400

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h3>Logo</h3>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+        </nav>
+      </header>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
