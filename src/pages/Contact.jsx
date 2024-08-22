f132bb177ad7c54e5ee1182227fba1b3a57fcea4
JD Tadlock <jt7903@gmail.com>
2024-08-22 13:33:05 -0400

    finished advanced form setup

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,46 +1,41 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-function Contact() {
-  const [fullName, setFullName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+const initialFormData = {
+  fullName: '',
+  email: '',
+  message: '',
+  stack: 'none',
+  access_key: '45ea0a30-6748-4a2f-b94b-e69469fa4e0b'
+}
 
+function Contact() {
+  const [formData, setFormData] = useState(initialFormData)
   const [alertMessage, setAlertMessage] = useState('')
 
   const handleSubmit = async (event) => {
     event.preventDefault()
 
     const url = 'https://api.web3forms.com/submit'
-    const accessKey = '45ea0a30-6748-4a2f-b94b-e69469fa4e0b'
 
-    const res = await axios.post(url, {
-      fullName,
-      email,
-      message,
-      access_key: accessKey
-    })
+    const res = await axios.post(url, formData)
 
     setAlertMessage('Your submission was received!')
-    setFullName('')
-    setEmail('')
-    setMessage('')
+    setFormData({
+      ...initialFormData
+    })
 
     setTimeout(() => {
       setAlertMessage('')
     }, 3500)
   }
 
-  const handleFullNameChange = (event) => {
-    setFullName(event.target.value)
-  }
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value)
-  }
-
-  const handleMessageChange = (event) => {
-    setMessage(event.target.value)
+  const handleInputChange = (event) => {
+    const dataProp = event.target.name
+    setFormData({
+      ...formData,
+      [dataProp]: event.target.value
+    })
   }
 
   return (
@@ -50,9 +45,17 @@ function Contact() {
       {alertMessage && <p className="text-center alert-message">{alertMessage}</p>}
 
       <form onSubmit={handleSubmit} className="contact-form column">
-        <input onChange={handleFullNameChange} value={fullName} name="fullName" type="text" placeholder="Enter your full name" />
-        <input onChange={handleEmailChange} value={email} name="email" type="email" placeholder="Enter your email address" />
-        <textarea onChange={handleMessageChange} value={message} name="message" rows="10" cols="30" placeholder="Enter your message"></textarea>
+        <input onChange={handleInputChange} value={formData.fullName} name="fullName" type="text" placeholder="Enter your full name" />
+        <input onChange={handleInputChange} value={formData.email} name="email" type="email" placeholder="Enter your email address" />
+        <label>Please choose the stack your are inquiring about:</label>
+
+        <select onChange={handleInputChange} name="stack" value={formData.stack}>
+          <option value="none" defaultValue={true}>None</option>
+          <option value="MERN">MERN</option>
+          <option value="MEAN">MEAN</option>
+          <option value="MEVN">MEVN</option>
+        </select>
+        <textarea onChange={handleInputChange} value={formData.message} name="message" rows="10" cols="30" placeholder="Enter your message"></textarea>
         <button>Send</button>
       </form>
     </>
